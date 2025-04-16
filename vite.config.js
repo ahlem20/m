b350dd53102149bee0b29cde34e09eab8c5e74f7
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2025-04-16 14:10:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+ server: {
+    proxy: {
+		 "/api": {
+			target: "https://morning-glory-backend.onrender.com",
+			 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
