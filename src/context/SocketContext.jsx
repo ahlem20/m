20a6a51dd8f549865daafe0ef0c2aba17adce206
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2025-04-17 20:17:44 +0100

    Update SocketContext.jsx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://morning-glory-backend.onrender.com", {
+			const socket = io("https://chat-app-yt.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
