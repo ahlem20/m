d19cd32438514172bd0ffad542d5f72f8cf016df
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2025-05-15 20:42:42 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.jsx"; // Make sure this is exported correctly
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <SocketContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SocketContextProvider>
-    </AuthProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<SocketContextProvider>
+					<App />
+				</SocketContextProvider>
+			</AuthContextProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
